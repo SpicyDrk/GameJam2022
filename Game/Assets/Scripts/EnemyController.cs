fcac5b8e84c0a7bdfdd559258f1c4f77b0c75bc9
Brian Mcgrath <brian.mcgrath@otised.com>
2022-05-24 10:44:34 -0400

    Work on animations and Screenbounds

--- Game/Assets/Scripts/EnemyController.cs
+++ Game/Assets/Scripts/EnemyController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,26 +7,38 @@ public class EnemyController : MonoBehaviour
     private float timeSinceLastHit = 100f;
     private bool moving = true;
     private Rigidbody2D rb;
+    private bool dying;
+    private Animator anim;
+    private CircleCollider2D cCollider;
+    private GameObject player;
+
+    private float timeToLiveAfterDeath = 3f;
 
     public float knockbackDistance = 10f;
     public float hitboxCooldown = 0.3f;
 
-    [SerializeField]
-    GameObject player;
+    [SerializeField] 
     public float startingHp = 100f;
     public float maxSpeed = 3f;
 
-    [SerializeField]
-    GameObject healthBar;
+    [SerializeField] GameObject healthBar, healthBarBackground;
     private float hp;
+
+    private float minX, maxX, minY, maxY, extendedBounds = 3f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        SetScreenBounds();
+        player = GameObject.FindGameObjectWithTag("Player");
         hp = startingHp;
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        cCollider = GetComponent<CircleCollider2D>();
     }
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.name == "Slash" && timeSinceLastHit > hitboxCooldown)
@@ -39,7 +49,7 @@ public class EnemyController : MonoBehaviour
             hp -= 20f;
             if (hp<= 0f)
             {
-                this.gameObject.SetActive(false);
+                StartDeath();
             }
             UpdateHealthBar();
         }
@@ -47,30 +57,85 @@ public class EnemyController : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        if (col.tag == "enemy")
+        if (col.tag == "enemy" && !dying)
         {
-            transform.position = Vector3.MoveTowards(transform.position, col.transform.position, -.05f);
+            transform.position = Vector3.MoveTowards(transform.position, col.transform.position, -.01f);
         }
+    }
+
+    private void StartDeath()
+    {
+        dying = true;
+        anim.SetBool("IsDying", true); 
+        moving = false;
+        rb.simulated = false;
+        cCollider.enabled = false;
+        healthBar.SetActive(false);
+        healthBarBackground.SetActive(false);
+        sr.material.SetFloat("_FlashAmount", 0f);
     }
 
     void Update()
     {
+        if (dying)
+        {
+            timeToLiveAfterDeath -= Time.deltaTime;
+            if (timeToLiveAfterDeath <= 0f)
+            {
+                gameObject.SetActive(false);
+            }
+            if (timeToLiveAfterDeath < 1f)
+            {
+                sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, timeToLiveAfterDeath / 1f);
+            }
+            return;
+        }
+
+        // for fixing Z plane issues
         Vector3 pos = transform.position;
         pos.z = 0;
         transform.position = pos;
+
         ShaderFader();
         if (moving)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, maxSpeed * Time.deltaTime);
             if (player.transform.position.x < transform.position.x)
             {
-                sr.flipX = true;// transform.localScale = new Vector3(-1f, 1f, 1f);
+                sr.flipX = true;
             } else
             {
-                sr.flipX = false;// transform.localScale = new Vector3(1f, 1f, 1f);
+                sr.flipX = false;
             }
         }
         timeSinceLastHit += Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        if (dying)
+            return;
+        
+        if (transform.position.x - player.transform.position.x  < minX)
+        {
+            var randomyY = UnityEngine.Random.Range(minY + transform.position.y + 1f, maxY + transform.position.y - 1f);
+            transform.position = new Vector2(maxX + player.transform.position.x -1f, randomyY);
+        }
+        if (transform.position.x - player.transform.position.x > maxX)
+        {
+            var randomyY = UnityEngine.Random.Range(minY + transform.position.y + 1f, maxY + transform.position.y-1f);
+            transform.position = new Vector2(minX + player.transform.position.x + 1f, randomyY);
+        }
+        if (transform.position.y - player.transform.position.y < minY)
+        {
+            var randomX = UnityEngine.Random.Range(minX + transform.position.x + 1f, maxX + transform.position.x - 1f);
+            transform.position = new Vector2(randomX, maxY + player.transform.position.y - 1f);
+        }
+        if (transform.position.y - player.transform.position.y > maxY)
+        {
+            var randomX = UnityEngine.Random.Range(minX + transform.position.x + 1f, maxX + transform.position.x - 1f);
+            transform.position = new Vector2(randomX, minY + player.transform.position.y + 1f);
+        }
     }
     void ShaderFader()
     {
@@ -87,8 +152,16 @@ public class EnemyController : MonoBehaviour
 
     void UpdateHealthBar()
     {
-        healthBar.transform.localScale = new Vector3(hp / startingHp, healthBar.transform.localScale.z, healthBar.transform.localScale.z);
+        var scale = hp < 0 ? 0f : hp / startingHp;
+        healthBar.transform.localScale = new Vector3(scale , healthBar.transform.localScale.z, healthBar.transform.localScale.z);
     }
 
-
+    void SetScreenBounds()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        maxX = bounds.x + extendedBounds;
+        minX = -bounds.x - extendedBounds;
+        minY = -bounds.y - extendedBounds;
+        maxY = bounds.y + extendedBounds;
+    }
 }
